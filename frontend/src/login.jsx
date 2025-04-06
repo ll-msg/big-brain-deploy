cc5429cb40872a7ce26f2be32d9363f3db3a2b02
Yanran Wang <z5295965@unsw.edu.au>
2025-04-06 14:55:36 +1000

    add css and login page

---
+++ frontend/src/login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import emailIcon from './assets/email.png'
+import passwordIcon from './assets/password.png'
+
+import './auth.css';
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    // login action
+    const handleLogin = async(e) => {
+        const res = await fetch('http://localhost:5005/admin/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+        if (!res.ok) {
+            const error = await res.json();
+            throw new Error(error.message || "Login Failed!");
+        }
+        const data = await res.json();
+        console.log(res)
+        // save token - need to change
+        localStorage.setItem("token", data.token);
+        console.log("Successfully login");
+        // navigate to home page after registration
+        navigate('/dashboard');
+    }
+
+    return (
+        <div className = 'container'>
+            <div className="header">
+                <div className='text'> Login </div>
+            </div>
+            <div className="inputs">
+                <div className='input'>
+                    <img src={emailIcon} alt="email" />
+                    <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div className='input'>
+                    <img src={passwordIcon} alt="password" />
+                    <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <div className='auth-link'>
+                    Haven't got an account? <a href="/register">Register here</a>
+                </div>
+                <div className="submit-container">
+                    <div className='submit' onClick={handleLogin}> Login </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
